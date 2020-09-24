b010f1a18e288e6c76ab19e8908214e33a9e1206
Scott Menzer <scott@id5.io>
2020-09-24 12:11:18 +0200

    delete the old cookies we used to set since with 0.9.3 we changed the name, but now publishers have both versions in their users' browsers

--- src/id5-api.js
+++ src/id5-api.js
@@ -50,6 +50,15 @@ ID5.init = function (options) {
       setTimeout(() => this.fireCallBack(), this.config.callbackTimeoutInMs);
     }
 
+    // TEMPORARY until all clients have upgraded past v0.9.3
+    // remove cookies that were previously set with the old cookie name
+    if (cfg.cookieName !== 'id5.1st') {
+      const expired = (new Date(Date.now() - 1000)).toUTCString();
+      utils.setCookie('id5.1st', '', expired);
+      utils.setCookie('id5.1st_last', '', expired);
+      utils.setCookie('id5.1st_nb', '', expired);
+    }
+
     if (storedResponse) {
       // this is needed to avoid losing the ID5ID from publishers that was
       // previously stored. Eventually we can remove this, once pubs have all
