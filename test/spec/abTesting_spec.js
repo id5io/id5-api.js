6b72ff3e032558fa1a15def45cde20b0677f846b
Scott Menzer <scott@id5.io>
2020-11-14 15:19:25 +0100

    add more tests around configuration validation and ensuring the control group would only ever be used when a/b testing is properly turned on

--- test/spec/abTesting_spec.js
+++ test/spec/abTesting_spec.js
@@ -11,39 +11,48 @@ describe('A/B Testing', function () {
     expect(abTesting.exposeId).to.be.a('function');
   });
 
-  describe('Configuration', function() {
+  describe('Configuration Validation', function() {
     beforeEach(function() {
       config.resetConfig();
     });
 
-    let testInvalidConfigs = [
-      { enabled: true, controlGroupPct: -1 },
-      { enabled: true, controlGroupPct: 2 },
-      { enabled: true, controlGroupPct: 'a' },
-      { enabled: true, controlGroupPct: true },
-      { enabled: true }
+    let testInvalidConfigsWithException = [
+      { abTesting: { enabled: true } },
+      { abTesting: { enabled: true, controlGroupPct: 2 } },
+      { abTesting: { enabled: true, controlGroupPct: -1 } },
+      { abTesting: { enabled: true, controlGroupPct: 'a' } },
+      { abTesting: { enabled: true, controlGroupPct: true } }
     ];
-    testInvalidConfigs.forEach((testConfig) => {
+    testInvalidConfigsWithException.forEach((testConfig) => {
       it('should throw error if config is invalid', function () {
-        config.setConfig({
-          abTesting: testConfig
-        });
+        config.setConfig(testConfig);
         expect(function () { abTesting.init() }).to.throw();
       });
     });
 
+    let testInvalidConfigsWithoutException = [
+      { abTesting: { enabled: false, controlGroupPct: -1 } },
+      { abTesting: { enabled: false, controlGroupPct: 2 } },
+      { abTesting: { enabled: false, controlGroupPct: 'a' } },
+      { abTesting: { enabled: false, controlGroupPct: true } }
+    ];
+    testInvalidConfigsWithoutException.forEach((testConfig) => {
+      it('should not throw error if config is invalid but A/B testing is off', function () {
+        config.setConfig(testConfig);
+        expect(function () { abTesting.init() }).to.not.throw();
+      });
+    });
+
     let testValidConfigs = [
-      { enabled: false, controlGroupPct: -1 },
-      { enabled: false, controlGroupPct: 2 },
-      { enabled: false, controlGroupPct: 'a' },
-      { enabled: false, controlGroupPct: true },
-      { enabled: false }
+      { abTesting: { } },
+      { abTesting: { enabled: false } },
+      { abTesting: { enabled: true, controlGroupPct: 0 } },
+      { abTesting: { enabled: true, controlGroupPct: 0.5 } },
+      { abTesting: { enabled: true, controlGroupPct: 1 } }
     ];
     testValidConfigs.forEach((testConfig) => {
-      it('should not throw error if config is invalid but A/B testing is off', function () {
-        config.setConfig({
-          abTesting: testConfig
-        });
+      it('should not throw error if config is valid', function () {
+        config.setConfig(testConfig);
         expect(function () { abTesting.init() }).to.not.throw();
       });
     });
@@ -52,52 +61,76 @@ describe('A/B Testing', function () {
   describe('Setting Control Group and Exposing ID', function() {
     let randStub;
 
-    beforeEach(function() {
-      randStub = sinon.stub(Math, 'random').callsFake(function() {
-        return 0.2;
-      });
-      config.resetConfig();
-    });
     afterEach(function () {
       randStub.restore();
     });
 
-    it('should expose ID when A/B config is not set', function () {
-      abTesting.init();
-      expect(abTesting.exposeId()).to.be.true;
+    describe('A/B Testing is not Explicitly Enabled', function() {
+      beforeEach(function() {
+        randStub = sinon.stub(Math, 'random').callsFake(function() {
+          return 0;
+        });
+        config.resetConfig();
+      });
+
+      it('should expose ID when A/B config is not set', function () {
+        abTesting.init();
+        expect(abTesting.exposeId()).to.be.true;
+      });
+
+      it('should expose ID when A/B config is not set and exposeId is called without init first', function () {
+        expect(abTesting.exposeId()).to.be.true;
+      });
+
+      it('should expose ID when A/B config is empty', function () {
+        config.setConfig({
+          abTesting: { }
+        });
+        abTesting.init();
+        expect(abTesting.exposeId()).to.be.true;
+      });
     });
 
-    it('should expose ID when A/B testing is off', function () {
-      config.setConfig({
-        abTesting: {
-          enabled: false,
-          controlGroupPct: 0.5
-        }
+    describe('A/B Testing Config Set', function () {
+      beforeEach(function() {
+        randStub = sinon.stub(Math, 'random').callsFake(function() {
+          return 0.25;
+        });
+        config.resetConfig();
       });
-      abTesting.init();
-      expect(abTesting.exposeId()).to.be.true;
-    });
 
-    it('should expose ID when not in control group', function () {
-      config.setConfig({
-        abTesting: {
-          enabled: true,
-          controlGroupPct: 0.1
-        }
+      it('should expose ID when A/B testing is off', function () {
+        config.setConfig({
+          abTesting: {
+            enabled: false,
+            controlGroupPct: 0.5
+          }
+        });
+        abTesting.init();
+        expect(abTesting.exposeId()).to.be.true;
       });
-      abTesting.init();
-      expect(abTesting.exposeId()).to.be.true;
-    });
 
-    it('should not expose ID when in control group', function () {
-      config.setConfig({
-        abTesting: {
-          enabled: true,
-          controlGroupPct: 0.5
-        }
+      it('should expose ID when not in control group', function () {
+        config.setConfig({
+          abTesting: {
+            enabled: true,
+            controlGroupPct: 0.1
+          }
+        });
+        abTesting.init();
+        expect(abTesting.exposeId()).to.be.true;
       });
-      abTesting.init();
-      expect(abTesting.exposeId()).to.be.false;
+
+      it('should not expose ID when in control group', function () {
+        config.setConfig({
+          abTesting: {
+            enabled: true,
+            controlGroupPct: 0.5
+          }
+        });
+        abTesting.init();
+        expect(abTesting.exposeId()).to.be.false;
+      });
     });
   });
 });
