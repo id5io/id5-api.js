afe9b08ce3673bbf957b589c9b553ff4a8cc0b6d
Paweł Kowalski <pkowalski@id5.io>
2025-10-14 15:35:36 +0200

    Revert "Event tracker: eids support"
    This reverts commit b720932245d4c581fab263d6c77e3a5d03ad4c4d.

--- test/spec/eventsTracker.spec.js
+++ test/spec/eventsTracker.spec.js
@@ -220,10 +220,8 @@ describe('registerEventsTracker', function () {
     };
 
     const partnerId = 321;
-    const result = await registerEventsTracker(prebidGlobal, partnerId);
-    expect(result).to.be.an('object');
-    expect(result.success).to.equal(false);
-    expect(result.reason).to.include('id5AnalyticsAdapter');
+    const reason = await registerEventsTracker(prebidGlobal, partnerId).catch((e) => String(e));
+    expect(reason).to.include('id5AnalyticsAdapter');
 
     expect(fetchStub).to.not.have.been.called;
     expect(prebidGlobal.onEvent).to.not.have.been.called;
@@ -336,16 +334,6 @@ describe('EventsTracker', function () {
       bidderRequests: [{
         bidderCode: 'bidderA',
         extraBRQ: 'x',
-        ortb2: {
-          user: {
-            ext: {
-              eids: [
-                {source: 'id5-sync.com', uids: [{ext: {linkType: 9, pba: 'brq', abTestingControlGroup: false}}]},
-                {source: 'other-source.com', uids: [{id: 'shouldNotBeIncluded'}]}
-              ]
-            }
-          }
-        },
         bids: [{
           adUnitCode: 'au-1',
           bidId: 'b-1',
@@ -359,20 +347,6 @@ describe('EventsTracker', function () {
               uid: "u2"
             }
           },
-          ortb2: {
-            user: {
-              ext: {
-                eids: [
-                  {source: 'id5-sync.com', uids: [{ext: {linkType: 7, pba: 'bid', abTestingControlGroup: true}}]},
-                  {source: 'another.com', uids: [{id: 'nope'}]}
-                ]
-              }
-            }
-          },
-          userIdAsEids: [
-            {source: 'id5-sync.com', uids: [{ext: {linkType: 5, pba: 'eids'}}]},
-            {source: 'third.com', uids: [{id: 'blocked'}]}
-          ],
           extraB: 'x'
         }]
       }],
@@ -408,16 +382,6 @@ describe('EventsTracker', function () {
       noBids: [{bidId: 'nb-1'}],
       bidderRequests: [{
         bidderCode: 'bidderA',
-        ortb2: {
-          user: {
-            ext: {
-              eids: [
-                {source: 'id5-sync.com', uids: [{ext: {linkType: 9, pba: 'brq', abTestingControlGroup: false}}]},
-                {source: 'other-source.com'}
-              ]
-            }
-          }
-        },
         bids: [{
           adUnitCode: 'au-1',
           bidId: 'b-1',
@@ -425,21 +389,7 @@ describe('EventsTracker', function () {
             id5id: {ext: {linkType: 1, pba: 'abc', abTestingControlGroup: true}},
             u1: 1,
             u2: 1
-          },
-          ortb2: {
-            user: {
-              ext: {
-                eids: [
-                  {source: 'id5-sync.com', uids: [{ext: {linkType: 7, pba: 'bid', abTestingControlGroup: true}}]},
-                  {source: 'another.com'}
-                ]
-              }
-            }
-          },
-          userIdAsEids: [
-            {source: 'id5-sync.com', uids: [{ext: {linkType: 5, pba: 'eids'}}]},
-            {source: 'third.com'}
-          ]
+          }
         }]
       }]
     });
